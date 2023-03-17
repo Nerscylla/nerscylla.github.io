78303f78ecdc19c49c4903e8195b08d14c1f958b
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2023-03-17 01:39:47 +0100

    changed font added scroll down button added light and dark mode

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const learningID = setInterval(updateLearning, 1000)
 let currentLearning = 0
+let lightModeActive = 0
+
+function init() {
+    document.querySelector(":root").classList.toggle("not-light")
+}
 
 function updateLearning() {
     let learning = ["Web Development", "Blender", "Linux", "Docker"]
@@ -9,4 +14,15 @@ function updateLearning() {
         currentLearning++
     }
     document.getElementById("greeting5").innerHTML = learning[currentLearning]
+}
+
+function toggleLightmode() {
+    document.querySelector(":root").classList.toggle("light")
+    document.querySelector(":root").classList.toggle("not-light")
+    lightModeActive++
+    if(lightModeActive % 2 == 1) {
+        document.getElementById("appearance").setAttribute("name", "moon")
+    } else {
+        document.getElementById("appearance").setAttribute("name", "sunny")
+    }
 }
