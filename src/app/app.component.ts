e28ad25179fe8b3424082e8a568c2043766a675d
TheBaum <the.baum12345@gmail.com>
2025-06-10 22:34:19 +0200

    feat: add project cards section with responsive grid layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
-	imports: [NavBarComponent, AboutMeComponent],
+	imports: [NavBarComponent, AboutMeComponent, ProjectCardComponent],
 })
 export class AppComponent {
 	title = 'About: Nerscylla';
