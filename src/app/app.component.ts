4c5f000d7f993d2f6047689069ad72fd860dce39
TheBaum <the.baum12345@gmail.com>
2025-06-15 22:46:24 +0200

    feat: add contact section with links to social media

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectCardComponent } from './project-card/project-card.component';
 import { SkillCardComponent } from './skill-card/skill-card.component';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+	simpleDiscord,
+	simpleGithub,
+	simpleInstagram,
+} from '@ng-icons/simple-icons';
+import { Title } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-root',
@@ -14,8 +20,14 @@ import { SkillCardComponent } from './skill-card/skill-card.component';
 		AboutMeComponent,
 		ProjectCardComponent,
 		SkillCardComponent,
+		NgIcon,
 	],
+	providers: [provideIcons({ simpleDiscord, simpleGithub, simpleInstagram })],
 })
 export class AppComponent {
-	title = 'About: Nerscylla';
+	title = 'About Nerscylla';
+
+	constructor(private titleService: Title) {
+		this.titleService.setTitle(this.title);
+	}
 }
